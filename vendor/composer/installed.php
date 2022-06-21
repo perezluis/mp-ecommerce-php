b230e827bef55b3144292c47ce910372668e486f
Luis Perez <perezluis197@gmail.com>
2022-06-21 16:54:30 -0600

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e3aedbfa369010f518359439ddcfb5c7c45a088c',
+    'name' => 'lp13/mp-ecommerce-php',
+  ),
+  'versions' => 
+  array (
+    'lp13/mp-ecommerce-php' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e3aedbfa369010f518359439ddcfb5c7c45a088c',
+    ),
+  ),
+);
